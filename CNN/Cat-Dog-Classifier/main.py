8743a77aa60db8665771e520e28a3b99127fcbf5
Rohan Hirekerur <rohanhirekerur@gmail.com>
2022-08-31 23:16:50 +0530

    Started modeling CNN [unstable]

---
+++ CNN/Cat-Dog-Classifier/main.py
@@ -0,0 +1,41 @@
+# Convolutional Neural Network
+
+# Importing the libraries
+from keras.preprocessing import image
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+tf.__version__
+
+# Part 1 - Data Preprocessing
+
+# Preprocessing the Training set
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+training_set = train_datagen.flow_from_directory('dataset/training_set',
+                                                 target_size=(64, 64),
+                                                 batch_size=32,
+                                                 class_mode='binary')
+
+# Preprocessing the Test set
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_datagen.flow_from_directory('dataset/test_set',
+                                            target_size=(64, 64),
+                                            batch_size=32,
+                                            class_mode='binary')
+
+# Part 4 - Making a single prediction
+
+test_image = image.load_img(
+    'dataset/single_prediction/cat_or_dog_1.jpg', target_size=(64, 64))
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = cnn.predict(test_image)
+training_set.class_indices
+if result[0][0] == 1:
+    prediction = 'dog'
+else:
+    prediction = 'cat'
+print(prediction)
